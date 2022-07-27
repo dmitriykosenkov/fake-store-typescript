b29a14c8de25ed20efd59d2cb786711d8e6ad57a
dmitriykosenkov <dmkos23011991@gmail.com>
2022-07-27 23:07:09 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { IProduct } from "./models";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [products, setProducts] = useState([]);
+
+   useEffect(() => {
+      const fetchProducts = async () => {
+         const response = await axios.get("https://fakestoreapi.com/products");
+         setProducts(response.data);
+      };
+      fetchProducts();
+   }, []);
+
+   return (
+      <div className="App">
+         <h1>Products</h1>
+         <ul>
+            {products.map((product: IProduct) => (
+               <li key={product.id}>
+                  <h4>{product.title}</h4>
+                  <img src={product.image} alt="" />
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
+};
 
 export default App;
